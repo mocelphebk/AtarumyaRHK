8d8955aa687d154f489091abb0383c430631c631
mocelphebk <104140734+mocelphebk@users.noreply.github.com>
2023-04-19 20:00:26 +0200

    Create convert_laion.py

--- convert_laion.py
+++ convert_laion.py
@@ -5,7 +5,6 @@ import csv
 input_file = 'laion_synthetic_filtered_large.json'
 output_file = 'laion_synthetic_filtered_large.tsv'
 
-# load JSON data from input file
 with open(input_file, 'r') as f:
     data = json.load(f)
 
@@ -14,7 +13,6 @@ header = data[0].keys()
 rows = [x.values() for x in data]
 
 # write data to TSV file
-with open(output_file, 'w') as f:
     writer = csv.writer(f, delimiter='\t')
     writer.writerow(header)
     writer.writerows(rows)
